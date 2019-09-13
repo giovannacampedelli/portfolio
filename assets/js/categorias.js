806a923fb1faf93646bbd2f702b7f75350ea46b9
Antonioarf <antonioarf@al.insper.edu.br>
2019-09-13 08:23:03 -0300

    rebve

--- assets/js/categorias.js
+++ assets/js/categorias.js
@@ -11,3 +11,20 @@ document.addEventListener('DOMContentLoaded', function() {
 
 /*nao colocar nada depois dessa chave */
 })
+
+/*
+{{#each bigfour}}
+        <li>
+          <p>
+            <strong>Chave:</strong> {{@key}}
+          </p>
+          <p>
+            <strong>Valor:</strong> {{this}}
+          </p>
+          <p>
+            <strong>Campo "nome" do valor:</strong> {{this.nome}}
+          </p>
+          <p>
+            <strong>Campo "numSlams" do valor:</strong> {{this.numSlams}}
+          </p>
+          */
