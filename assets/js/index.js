d2a40d2c8c6ca29e1ec2a4e6d72f6d6eaaff12fb
Antonioarf <antonioarf@al.insper.edu.br>
2019-09-13 07:48:54 -0300

    vfdd

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+	let params = coDesExtract()
+	console.log(params)
+	
+	let db = coDesConnect('https://avengers-2.firebaseio.com/')
+
+	let location {
+    origin: "pagina_principal", }
+	
+	console.log(window.location)
+
+/*nao colocar nada depois dessa chave */
+})
