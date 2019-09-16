92f57e139eaaee109bb6f1c5e46f36b18c06b8ac
Giovanna Campedelli <giovannahc@al.insper.edu.br>
2019-09-16 20:56:00 -0300

    new

--- assets/js/categorias.js
+++ assets/js/categorias.js
@@ -5,16 +5,26 @@ document.addEventListener('DOMContentLoaded', function() {
   let db = coDesConnect('https://avengers-2.firebaseio.com/')
 
   db.download('/', function(data) {
+    console.log(data)
 
+    /*
+      NUNCA, JAMAIS, EM HIPÓTESE ALGUMA faça
+      replace em múltiplas tags se alguma
+      estiver dentro de outra! Veja o HTML
+      e note que não é o caso das abaixo.
 
-    context = data['projetos'][value]
-    coDesReplace('.grupos', context)
+      É sério. Se fizer isso, você realmente
+      vai se arrepender. Você foi avisado.
+    */
+
+    context = data['biblioteca'][value]
+    coDesReplace('title', context)
 
     context = data
     coDesReplace('.grupos', context)
 
     context = data['biblioteca'][value]
-    coDesReplace('.call', context)
+    coDesReplace('.texto', context)
 
     context = data['biblioteca'][value]
     coDesReplace('.book-list', context)
