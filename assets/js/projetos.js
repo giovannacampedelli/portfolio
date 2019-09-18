418aca2494f390897667e0601651a7c7844374ee
Giovanna Campedelli <giovannahc@al.insper.edu.br>
2019-09-18 10:49:32 -0300

    s

--- assets/js/projetos.js
+++ assets/js/projetos.js
@@ -5,12 +5,20 @@ document.addEventListener('DOMContentLoaded', function() {
 
   let db = coDesConnect('https://avengers-2.firebaseio.com/')
 
+
   db.download('/', function(data) {
   	coDesReplace('.app_menu', data)
+
+    
     
     context = data['portfolio'][value1]['projetos'][value2]
     console.log(context)
+
     coDesReplace('.vazio', context)
+    coDesReplace('.caminho_item', context)
+    coDesReplace('.link_', context)
+    coDesReplace('title', context)
+
 
 
     // coDesReplace('.title_2', context)
